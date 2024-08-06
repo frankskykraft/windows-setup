ca617c92ecbd585988bfaf28df35ece82dfe1aa9
Frank Iannelli <frank.iannelli@skykraft.com.au>
2024-08-07 09:46:33 +1000

    update readme

---
+++ README.md
@@ -0,0 +1,14 @@
+Download the script
+
+Press Windows + X and select Windows PowerShell (Admin).
+```
+cd $HOME\Downloads
+```
+
+```
+Set-ExecutionPolicy Bypass -Scope Process -Force
+```
+
+```
+.\install-python.ps1
+```
--- install-python.ps1
+++ install-python.ps1
@@ -1,24 +1,19 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-$scriptPath = "$env:TEMP\install_python.ps1"
-Set-Content -Path $scriptPath -Value @'
-# Define the URL for the latest Python installer
 $pythonInstallerUrl = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
 
-# Define the path to save the installer
 $installerPath = "$env:TEMP\python-3.12.0-amd64.exe"
 
-# Download the Python installer
 Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $installerPath
 
-# Run the installer silently
-Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -NoNewWindow -Wait
+Start-Process -FilePath $installerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=0" -NoNewWindow -Wait
 
-# Clean up the installer file
 Remove-Item -Path $installerPath
 
-# Verify the installation
-python --version
-'@
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[-1]
 
-# Run the script
-. $scriptPath
+$pythonInstallPath = "C:\Users\$currentUser\AppData\Local\Programs\Python\Python312"  # Adjust version number if necessary
+
+$env:Path += ";$pythonInstallPath"
+[System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+
+python --version
+
