b27e1e01be2402a51c9ab7a0737524ecbc2abf41
Frank Iannelli <frank.iannelli@skykraft.com.au>
2024-08-07 10:14:39 +1000

    update script

--- install-python.ps1
+++ install-python.ps1
@@ -1,6 +1,5 @@
-
 # Define the URL for the latest Python installer
-$pythonInstallerUrl = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
+$pythonInstallerUrl = "https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe"
 
 # Define the path to save the installer
 $installerPath = "$env:TEMP\python-3.12.0-amd64.exe"
@@ -20,21 +19,23 @@ Write-Output "Cleaning up the installer file..."
 Remove-Item -Path $installerPath
 Write-Output "Installer file removed"
 
-# Get the current user's username
-$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[-1]
-Write-Output "Current user: $currentUser"
-
 # Define the Python installation path
-$pythonInstallPath = "C:\Users\$currentUser\AppData\Local\Programs\Python\Python312"  # Adjust version number if necessary
+$pythonInstallPath = "C:\Program Files\Python312"
 Write-Output "Python installation path: $pythonInstallPath"
 
-# Add Python to the system PATH
-Write-Output "Adding Python to the system PATH..."
-$env:Path += ";$pythonInstallPath"
-[System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-Write-Output "Python added to the system PATH"
+# Verify if Python was installed in the expected directory
+if (Test-Path -Path "$pythonInstallPath\python.exe") {
+    Write-Output "Python executable found at $pythonInstallPath\python.exe"
+    
+    # Add Python to the system PATH
+    Write-Output "Adding Python to the system PATH..."
+    $env:Path += ";$pythonInstallPath"
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+    Write-Output "Python added to the system PATH"
 
-# Verify the installation
-Write-Output "Verifying Python installation..."
-python --version
+    # Open a new PowerShell window to verify the installation
+    Write-Output "Please open a new PowerShell window and run 'python --version' to verify the installation."
+} else {
+    Write-Output "Python executable not found at $pythonInstallPath. Installation might have failed."
+}
 
